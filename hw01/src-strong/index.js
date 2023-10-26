01c0ebe0afcc2c3a36e450a1c06584ee9cd5504f
Yu-Wei, Lyu <awaia732@gmail.com>
2023-10-27 00:56:03 +0800

    hw01 strong baseline remove yAxis

--- hw01/src-strong/index.js
+++ hw01/src-strong/index.js
@@ -1,13 +1,7 @@
 // https://observablehq.com/@shaunkallis/stacked-horizontal-bar-chart-of-cumulative-homework-score
 // 參考 D3 畫廊
-// 測試分數和顏色的數據
-const testScores = ['Test A', 'Test B', 'Test C'];
-const colors = ['#ff0000', '#00ff00', '#0000ff'];
 
-// student-scores-in-homeworks
-// data
 d3.text("../data/csv/data.csv").then(function (data) {
-//d3.text("../data/csv/student-scores-in-homeworks.csv").then(function (data) {
     const parsed_csv = d3.csvParse(data, (d, i, columns) => {
         d3.autoType(d); // 自動檢測並轉換數據類型
         d.total = d3.sum(columns.slice(5), c => d[c]); // 計算總分
@@ -18,14 +12,25 @@ d3.text("../data/csv/data.csv").then(function (data) {
     
     series = d3.stack().keys(parsed_csv.columns.slice(5))(parsed_csv).map(d => (d.forEach(v => v.key = d.key), d))
     //console.log(series)
-    margin = ({top: 30, right: 10, bottom: 0, left: 112});
+    margin = ({top: 30, right: 100, bottom: 0, left: 250});
 
     width = 900
     height = parsed_csv.length * 25 + margin.top + margin.bottom;
 
     formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en");
+    //序號,班級,學號,姓名,GitHub 帳號
+    y = d3.scaleBand()
+        .domain(parsed_csv.map(d => d))
+        .range([margin.top, height - margin.bottom])
+        .padding(0.08);
 
+    x = d3.scaleLinear()
+        .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
+        //.domain([0, 100])
+        .range([margin.left, width - margin.right]);
+     
     yAxis = g => g.attr("transform", `translate(${margin.left},0)`)
+        .attr("id", "yAxis")
         .call(d3.axisLeft(y).tickSizeOuter(0))
         .call(g => g.selectAll(".domain").remove());
 
@@ -36,17 +41,6 @@ d3.text("../data/csv/data.csv").then(function (data) {
     color = d3.scaleOrdinal()
         .domain(series.map(d => d.key))
         .range(d3.schemeSpectral[series.length] || ["#ccc"]);
-
-    y = d3.scaleBand()
-        .domain(parsed_csv.map(d => d.姓名))
-        .range([margin.top, height - margin.bottom])
-        .padding(0.08);
-
-    x = d3.scaleLinear()
-        //.domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
-        .domain([0, 100])
-        .range([margin.left, width - margin.right]);
-
     score_svg = d3.create("svg")
         .attr("viewBox", [0, 0, width, height]);
     
@@ -60,19 +54,17 @@ d3.text("../data/csv/data.csv").then(function (data) {
         .data(d => d)
         .join("rect")
         .attr("x", d => x(d[0]))
-        .attr("y", (d, i) => y(d.data.姓名))
+        .attr("y", (d, i) => y(d.data))
         .attr("width", d => x(d[1]) - x(d[0]))
         .attr("height", y.bandwidth())
         .append("title")
         .text(d => `${d.data.name} ${d.key} ${formatValue(d.data[d.key])});`)
     
-    score_svg.select("#chart")
+    // score_svg.append("g")
+    //     .call(xAxis);
     
     // score_svg.append("g")
-    // .call(xAxis);
-
-    score_svg.append("g")
-    .call(yAxis);
+    //     .call(yAxis);
 
     // 添加 text 元素，確保它在 rect 之後
     score_svg.select("#chart")
@@ -83,7 +75,7 @@ d3.text("../data/csv/data.csv").then(function (data) {
         .data(d => d)
         .join("text")
         .attr("x", d => x(d[0]) + (x(d[1]) - x(d[0])) / 2)
-        .attr("y", (d, i) => y(d.data.姓名) + 16)
+        .attr("y", (d, i) => y(d.data) + 16)
         .attr("text-anchor", "middle")
         .attr("fill", "white")
         .attr("font-family", "'Noto Serif JP', serif")
@@ -99,8 +91,10 @@ d3.text("../data/csv/data.csv").then(function (data) {
         });
     
     d3.select("#div2")
-        .attr("class", "div-hint")
-        .node().appendChild(score_svg.node());
+        .append("div")
+        //.attr("class", "div-hint")
+        .node()
+        .appendChild(score_svg.node());
 
     hint_block_height = 10
     hint_block_width = 50
@@ -136,6 +130,8 @@ d3.text("../data/csv/data.csv").then(function (data) {
         .append("text")
         .text("作業顏色對照表")
         .node().appendChild(hint_svg.node());
+
+
 });
 
 //***************************************************
